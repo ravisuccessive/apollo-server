7072217781cc99ec4e984127105963acee431484
itadmin <itadmin@ravi.lapr218.successivetech.com>
2021-02-08 17:44:38 +0530

    feat:Subscriptions

--- src/modules/trainee/index.js
+++ src/modules/trainee/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export { default as Query } from './query';
 export { default as Mutation } from './mutation';
 export { default as subscription } from './subscription';
--- src/modules/trainee/mutation.js
+++ src/modules/trainee/mutation.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import userInstance from '../../service/user';
 import pubsub from '../pubsub';
 import constants from '../../libs/constants';
